29ce9f1fed0fe115408955002c62308a1aa20f87
Vsevolod Remizov <vsevolod.remizov@gmail.com>
2024-02-19 02:31:48 +0300

    Database added

---
+++ program/SolitaireDB/Database.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitaireDB
+{
+    public class Database
+    {
+        string conString = "";
+        public Database() { }
+
+        Account GetAccount(int id) //получить запись с данными аккаунта
+        {
+            return new Account();
+        }
+
+        void SaveAccount(Account account)
+        {
+            //сохранить аккаунт
+        }
+    }
+}
