1a86d5c2f78854218865608f2d644d7ade467da7
netdev2 <netdev2@private.social>
2024-02-19 00:59:07 +0300

    Solitaire project added

---
+++ program/Solitaire/Pyramid.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solitaire
+{
+    public class Pyramid: Solitaire //Пирамида
+    {
+        public Pyramid() { }
+        void FillField()
+        {
+            //расположение карт «пирамидой»
+        }
+        void Move()
+        {
+            //сделать ход
+        }
+
+        void MoveCancel()
+        {
+            //отмена хода
+        }
+        void MoveCheck()
+        {
+            //проверка, есть ли доступные ходы
+        }
+        void Solve()
+        {
+            //автоматическое решение пасьянса
+        }
+        void Hint()
+        {
+            //подсказка
+        }
+
+        void CardMatch()
+        {
+            //сопоставление карт (карта может быть размещена только на карту большего достоинства и другого цвета или на пустое место, группы карт перемещаются аналогично).
+        }
+
+        void CardBlock()
+        {
+            //блокировка карты
+        }
+    }
+}
---
+++ program/Solitaire/Solitaire.cs
@@ -0,0 +1,49 @@
+﻿using Solitaire.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Solitaire
+{
+    public class Solitaire //основная логика, общая для всех видов
+    {
+        List<Card> deck { get; set; } // колода
+        List<Card[]> field { get; set; } // игровое поле
+        Timer timer; // время хода
+        int score;  // очки
+        int move; // ходы
+        void Sort()
+        {
+            //сортировка колоды
+        }
+
+        void Draw()
+        {
+            //раздача (случайное заполнение deck)
+        }
+        void GetScorePoints()
+        {
+            //подсчет очков
+        }
+        
+        void GetGameTime()
+        {
+            //подсчет времени игры
+        }
+
+        void GetMoveCount()
+        {
+            //подсчет количества ходов
+        }
+    }
+
+    public class Card //карта = масть + достоинство
+    {
+        public CardSuitEnum suit { get; set;}
+        CardValueEnum value { get; set; }
+        bool bloked { get; set; } = false; //для Пирамиды
+    }
+}
