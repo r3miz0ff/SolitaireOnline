f0754dfe5081cbfa530872c67f34fadb4c8cb420
Vsevolod Remizov <vsevolod.remizov@gmail.com>
2024-02-19 02:25:19 +0300

    Account added

---
+++ program/SolitaireDB/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolitaireDB
+{
+    public class Account
+    {
+        int id { get; set; }
+        string name { get; set; }   
+        string statistics { get; set; }
+        public Account() { }
+    }
+}
